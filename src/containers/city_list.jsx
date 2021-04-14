d8b729d6d5ba316dbd213562cb8b397c94993b05
danburck <daniel.m.burckhardt@gmail.com>
2021-04-14 15:08:41 +0200

    create redux state and map setCities action and redux state to citiesList Container

--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { setCities } from '../actions/index';
 
 import City from '../containers/city';
+
+// wire action to the props of the component
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { setCities: setCities },
+    dispatch
+  );
+}
+
+// wire Redux state to the props of the component
+function mapStateToProps(state) {
+  return {
+    cities: state.cities
+  };
+}
 
 class CityList extends Component {
   render() {
@@ -14,6 +32,10 @@ class CityList extends Component {
       </div>
     );
   }
+
+  componentWillMount() {
+    this.props.setCities();
+  }
 }
 
-export default CityList;
+export default connect(mapStateToProps, mapDispatchToProps)(CityList);
